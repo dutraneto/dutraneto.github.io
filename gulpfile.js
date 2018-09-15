1e43a0ddf6342645e9f2309f175a53d6a9919b83
dutraneto <dudutraneto@gmail.com>
2018-09-15 10:15:27 -0400

    prepare some files to responsive mode

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,29 @@ const gulp = require("gulp"),
 	shell = require("gulp-shell"),
 	browserSync = require("browser-sync").create(),
 	del = require("del");
-const siteRoot = "_site/";
+const siteRoot = "_site";
 
 /* Tasks cached */
 gulp.task("cache:css", function() {
 	del("./_site/assets/css/main.css")
 });
 
-// task for building blog when something changed. Choose port: 3000 because of bs
-gulp.task("build", shell.task(["bundle exec jekyll clean && bundle exec jekyll serve"]));
+// task for building blog when something changed.
+gulp.task("build", shell.task(["jekyll build --watch --incremental"]));
 
 //task for serving blog with browsersync
 gulp.task("serve", function () {
 	browserSync.init({
-		files: [siteRoot + '**'],
+		port: 4000,
 		server: { baseDir: siteRoot },
 		notify: true,
 	});
 	// Reloads page when some of the already built files changed:
-    gulp.watch('_site/**/*.*').on('change', browserSync.reload);
-})
+	setTimeout(function () {
+		gulp.watch('_site/**/*.*').on('change', browserSync.reload);
+		gulp.watch('*.html').on('change', browserSync.reload);
+	}, 5000);
+});
 
 gulp.task("default", ["build", "serve"]);
 
